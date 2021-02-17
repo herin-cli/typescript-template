84a79c19bd44cb6e695c25e8c89fc63baaa04cc2
kilicmu <kilicmu3389@outlook.com>
2021-02-17 19:15:47 +0800

    init

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    presets: [
+        ["@babel/preset-env", {
+            useBuiltIns: false,
+            corejs: {version: 3},
+            targets: ">0.25%"
+        }],
+        "@babel/preset-react",
+    ],
+    plugins: [],
+}
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const obj = {
+    name: 1,
+    age: 2,
+};
+
+function a() {
+    console.log('hel');
+    return 1;
+}
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
